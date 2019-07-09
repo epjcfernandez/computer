dab2cdc9c95bf6b4b069f32d82ec6d6e2b27d928
epjcfernandez <51954401+epjcfernandez@users.noreply.github.com>
2019-07-09 09:27:46 +0800

    Add files via upload

--- Experiment 3-2.cpp
+++ Experiment 3-2.cpp
@@ -2,19 +2,19 @@
 #include <conio.h>
 using namespace std;
 
-const char PROVINCE = 3;
-const int WEEK = 7;
+const char province = 3;
+const int week = 7;
 
 int main()
 {
-    int temperature[PROVINCE][WEEK];
+    int temperature[province][week];
 
     cout << "Enter all temperature for a week of Province A, Province B and then Province C. \n";
 
     
-    for (char n = 0; n < PROVINCE; ++n)
+    for (char n = 0; n < province; ++n)
     {
-        for(int x = 0; x < WEEK; ++x)
+        for(int x = 0; x < week; ++x)
         {
             cout << "PROVINCE " << n + 1 << ", Day " << x + 1 << " : ";
             cin >> temperature[n][x];
@@ -24,15 +24,15 @@ int main()
     cout << "\n\nDisplaying Values:\n";
 
    
-    for (char n = 0; n < PROVINCE; ++n)
+    for (char n = 0; n < province; ++n)
     {
-        for(int x = 0; x < WEEK; ++x)
+        for(int x = 0; x < week; ++x)
         {
             cout << "PROVINCE " << n + 1 << ", Day " << x + 1 << " = " << temperature[n][x] << endl;
         }
     }
 
 
-	  getch();
+	 getch();
     return 0;
 }
